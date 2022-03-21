d8ae768169b730ebfbb0f9958cac1e26fa0e8824
Tahir Uzelli <58370244+tahiruzelli@users.noreply.github.com>
2022-03-21 17:00:06 +0300

    project init and model layer created

---
+++ AID/Model/Avatar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AID.Model
+{
+    public class Avatar
+    {
+        public int id { get; set; }
+        public string avatarUrl { get; set; }
+        public string createTime { get; set; }
+
+    }
+}
---
+++ AID/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AID.Model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string avatarUrl { get; set; }
+        public double balance { get; set; }
+        public double totalGain { get; set; }
+        public double totalVideoEditetTime { get; set; }
+        public string createDate { get; set; }
+        
+
+
+
+    }
+}
